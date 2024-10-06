e844f5e7798faffe837379c871577214cf1f7eaf
Adam Griffin <ag@agriffin.me>
2024-10-05 18:54:33 -0700

    Added auth service
    Just a simple service to test out connecting to service with Tilt and Kubernetes. The name "auth" is from the example repo. It doesn't really do any authentication.

--- Tiltfile
+++ Tiltfile
@@ -1,4 +1,15 @@
 docker_build(
+  'adamwgriffin/auth',
+  context='auth',
+  dockerfile='auth/Dockerfile',
+  live_update=[
+    sync('./auth', '/app')
+  ]
+)
+
+k8s_yaml('./infra/k8s/auth-depl.yml')
+
+docker_build(
   'adamwgriffin/my-nextjs-app-img',
   context='client',
   dockerfile='client/Dockerfile',
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,25 @@
+'use client'
+
+import { FormEvent } from 'react'
 import styles from './page.module.css'
 
 export default function Home() {
+  const onSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+    const res = await fetch('/api/users/currentuser')
+    console.log(res)
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Testing</h1>
         {/* <p>Hello, world! 🤘</p> */}
+        <form onSubmit={onSubmit}>
+          <button className='btn btn-primary'>
+            Test Endpoint
+          </button>
+        </form>
       </main>
     </div>
   )
