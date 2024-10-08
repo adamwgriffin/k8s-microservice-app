8ee3b2e74487217ddc78dc0a8617a1a48aaf0522
Adam Griffin <ag@agriffin.me>
2024-10-07 22:16:54 -0700

    Added all the environment variables

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -14,7 +14,18 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Testing</h1>
-        {/* <p>Hello, world! 🤘</p> */}
+        <h2>NEXT_PUBLIC enviroment variable added to build:</h2>
+        <ul>
+          <li>
+            NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:{' '}
+            {process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+          </li>
+          <li>
+            NEXT_PUBLIC_GOOGLE_MAPS_ID: {process.env.NEXT_PUBLIC_GOOGLE_MAPS_ID}
+          </li>
+          <li>NEXT_PUBLIC_LOCALE: {process.env.NEXT_PUBLIC_LOCALE}</li>
+          <li>NEXT_PUBLIC_CURRENCY: {process.env.NEXT_PUBLIC_CURRENCY}</li>
+        </ul>
         <form onSubmit={onSubmit}>
           <button className='btn btn-primary'>
             Test Endpoint
