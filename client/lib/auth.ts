71b8fca9fcdcaf394d1ee10e508fa8364ce08e5a
Adam Griffin <ag@agriffin.me>
2024-10-21 19:08:52 -0700

    Fixed bug with fetch refactor

--- client/lib/auth.ts
+++ client/lib/auth.ts
@@ -1,12 +1,15 @@
 import { queryOptions } from '@tanstack/react-query'
 import { CurrentUserResponse } from '../types'
 
-export async function getCurrentUser(): Promise<CurrentUserResponse> {
+export async function getCurrentUser(): Promise<
+  CurrentUserResponse['currentUser']
+> {
   const response = await fetch('/api/users/current_user')
   if (!response.ok) {
     throw new Error(`Network response was not ok. status: ${response.status}`)
   }
-  return response.json()
+  const data = (await response.json()) as CurrentUserResponse
+  return data.currentUser
 }
 
 export const getCurrentUserQueryOptions = queryOptions({
