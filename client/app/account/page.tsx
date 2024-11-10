7b9278b7021a628463e72810a37d8464bd1e56a4
Adam Griffin <ag@agriffin.me>
2024-11-09 23:16:26 -0800

    Removed client code that was causing build error
    We can't use the store on this page since it's not a client component. Just got rid of it for now since it was just for testing purposes to begin with.

--- client/app/account/page.tsx
+++ client/app/account/page.tsx
@@ -1,20 +1,8 @@
-import { useAppStore } from '../providers/AppStoreProvider'
-
 export default function Page() {
-  const currentUser = useAppStore((state) => state.currentUser)
-
   return (
     <>
       <h1>Account</h1>
       <p>This route requires auth.</p>
-      <p>
-        It should also get the currently logged in user on the server via
-        headers set in middleware. The only drawback is that fetching the
-        headers this way opts the page into dynamic rendering.
-      </p>
-      <h2>Logged In User</h2>
-      <p>ID: {currentUser?.id}</p>
-      <p>Email: {currentUser?.email}</p>
     </>
   )
 }
